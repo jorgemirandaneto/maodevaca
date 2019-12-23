586736b1344655e2c3f6fac4aa3e0d7856904364
jorgemirandaneto <jorgemirandaneto@live.com>
2019-12-23 15:48:22 -0300

    Atualizacao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '1067672898:AAEjorGizWrVYskfznYdtYwDAqyhiT6G25Y'
 const bot = new TelegramBot(TOKEN, { polling: true })
+const port = process.env.PORT || 3000
 
 bot.on('text', (msg) => {
     console.log(msg)
