1e38b93b7745bb905e9cb273771ec8dc76eba6f3
jorgemirandaneto <jorgemirandaneto@live.com>
2019-12-23 11:53:10 -0300

    Correcao porta

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const porta = process.env.PORT || 3000
+
 const TelegramBot = require( `node-telegram-bot-api` )
 
 const TOKEN = `1067672898:AAEjorGizWrVYskfznYdtYwDAqyhiT6G25Y`
@@ -5,6 +7,6 @@ const TOKEN = `1067672898:AAEjorGizWrVYskfznYdtYwDAqyhiT6G25Y`
 const bot = new TelegramBot( TOKEN, { polling: true } )
 
 bot.on('new_chat_members', (msg) => {
-    console.log(msg)
+    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, Seja bem vindo`)
  })
 
