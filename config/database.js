70097eed13596549feb3ff4e94a5a04d3bb87126
jorge miranda <jorgemirandaneto@gmail.com>
2020-01-05 22:02:46 -0300

    Criaçao do start

--- config/database.js
+++ config/database.js
@@ -1,21 +1,12 @@
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_DATABASE, process.env.PASSWORD_DATABASE, {
-//     host: process.env.HOST,
-//     dialect: process.env.DIALECT,
-//     define: {
-//         underscored: true
-//     }
-// })
-
-const sequelize = new Sequelize('aziyewem', 'aziyewem', '4P2mTfe11A60R1WGF9smjanZWbiCpb7r', {
-    host: 'elmer.db.elephantsql.com',
-    dialect: 'postgres',
-    define:{
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_DATABASE, process.env.PASSWORD_DATABASE, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    define: {
         underscored: true
     }
 })
-
 
 sequelize
     .authenticate()
@@ -34,11 +25,5 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 db.card = require('../models/card')(sequelize, Sequelize);
-// db.mesuare = require('../models/mesuare')(sequelize, Sequelize);
-// db.user = require('../models/user')(sequelize, Sequelize);
-
-// db.item.belongsTo(db.mesuare);
-// db.mesuare.hasMany(db.item);
-
 
 module.exports = db;
